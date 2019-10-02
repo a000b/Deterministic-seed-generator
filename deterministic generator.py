9c8c3e8fad5356a391de8affe8fe0a7c3e183614
a000b <46565897+a000b@users.noreply.github.com>
2019-10-02 17:41:58 +0200

    Add files via upload

---
+++ deterministic generator.py
@@ -0,0 +1,12 @@
+lineList = [line.rstrip('\n') for line in open('bip39_en_words.txt')]
+p = 31 #słowo startowe
+s = 78 #skok
+for i in range(24):
+    try:
+        print(i + 1, p, lineList[p])
+        p += s
+    except IndexError:
+        print("poza zakresem, dostosuj parametry")
+        break 
+
+
